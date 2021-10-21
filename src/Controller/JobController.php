7cd29a25a1df1a88c17754fb868e46c53d9db303
aymentou <infoingepi@gmail.com>
2021-10-21 12:47:52 +0200

    projet v

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -27,12 +27,34 @@ class JobController extends AbstractController
     public function acceuil(): Response 
     {
     
-        return $this->render('job/index.html.twig', [
+        return $this->render('acceuil/index.html.twig', [
             'controller_name' => 'JobController',
         ]);
     }
 
+/**
+     * @Route("/voir/{id}", name="voir" ,requirements ={"id"="\d+"})
+     * 
+     *
+     */
 
+    public function voir($id): Response 
+    {
+    
+        return new Response("hello page voir avec id".$id);
+    }
 
+    /**
+     * @Route("/voir", name="voir" )
+     * 
+     *
+     */
 
+    public function voirfun(): Response 
+    {
+    
+        return $this->render('voir.html.twig', [
+            'controller_name' => 'JobController',
+        ]);
+    }
 }
